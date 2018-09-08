91bd817fccc2fb5b39f4e7c20533646774cf7494
ejmoon <ejmoon09@gmail.com>
2018-09-08 16:48:59 +0900

    b3

--- testHello/src/testHello/testGit2.java
+++ testHello/src/testHello/testGit2.java
@@ -4,9 +4,9 @@ public class testGit2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("he");
+		System.out.println("he333");
 		System.out.println("world");
-		System.out.println("kkk");
+		System.out.println("kkk222");
 		System.out.println("??");
 		System.out.println("hello");
 	}
